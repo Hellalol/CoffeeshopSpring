b4cf854372de676242465b3e8feb33aed36029bc
Fredrik Billing <frhabi@gmail.com>
2020-05-06 17:16:43 +0200

    Initial push

---
+++ src/main/java/com/example/coffeeshop/CoffeeshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeshopApplication.class, args);
+    }
+
+}
