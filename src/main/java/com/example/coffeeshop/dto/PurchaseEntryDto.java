59f8f6294d58c5213a74d9007cad5d11b7afd2a8
Victor Olofsson <victor.olofsson@yh.nackademin.se>
2020-05-10 10:35:02 +0200

    A couple of basic DTOs.

---
+++ src/main/java/com/example/coffeeshop/dto/PurchaseEntryDto.java
@@ -0,0 +1,25 @@
+package com.example.coffeeshop.dto;
+
+import com.example.coffeeshop.domain.Product;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+
+public final class PurchaseEntryDto {
+    private final Product product; // TODO Change to id or DTO
+    @With
+    private final int quantity;
+    @With
+    private final BigDecimal currentPrice;
+
+    public PurchaseEntryDto(Product product) {
+        this.product = product;
+        this.quantity = 0;
+        this.currentPrice = product.getBasePrice();
+    }
+}
--- src/main/java/com/example/coffeeshop/service/ShoppingCart.java
+++ src/main/java/com/example/coffeeshop/service/ShoppingCart.java
@@ -4,55 +4,62 @@ import com.example.coffeeshop.domain.Customer;
 import com.example.coffeeshop.domain.Product;
 import com.example.coffeeshop.domain.Purchase;
 import com.example.coffeeshop.domain.PurchaseEntry;
+import com.example.coffeeshop.dto.PurchaseEntryDto;
+import lombok.Getter;
 import lombok.ToString;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @ToString
 // TODO Adapt to REST statelessness
 // TODO Move to somewhere more appropriate
+// TODO Implement discount logic, here or elsewhere
 public class ShoppingCart {
-    private final Customer workingCustomer;
-    private final Map<Product, PurchaseEntry> workingEntries;
-    private final Purchase workingPurchase;
+    @Getter
+    private final Customer customer;
+    private final Map<Product, PurchaseEntryDto> workingEntries;
 
     public ShoppingCart(Customer customer) {
-        this.workingCustomer = customer;
+        this.customer = customer;
         this.workingEntries = new TreeMap<>(Comparator.comparing(Product::getId));
-        this.workingPurchase = new Purchase(workingCustomer);
-    }
-
-    public Customer getCustomer() {
-        return workingCustomer;
     }
 
     public void increment(Product product) {
-        PurchaseEntry entry = workingEntries.getOrDefault(product, new PurchaseEntry(workingPurchase, product, 0, product.getBasePrice()));
-        entry.setQuantity(entry.getQuantity() + 1);
+        PurchaseEntryDto entry = workingEntries.getOrDefault(product, new PurchaseEntryDto(product));
+        entry = entry.withQuantity(entry.getQuantity() + 1);
         workingEntries.put(product, entry);
     }
 
     public void decrement(Product product) {
         if (workingEntries.containsKey(product)) {
-            PurchaseEntry entry = workingEntries.get(product);
-            entry.setQuantity(Math.max(0, entry.getQuantity() - 1));
+            PurchaseEntryDto entry = workingEntries.get(product);
+            workingEntries.put(product, entry.withQuantity(Math.max(0, entry.getQuantity() - 1)));
         }
     }
 
     public void zeroOut(Product product) {
         if (workingEntries.containsKey(product)) {
-            PurchaseEntry entry = workingEntries.get(product);
-            entry.setQuantity(0);
+            workingEntries.put(product, workingEntries.get(product).withQuantity(0));
         }
     }
 
     public void clearStaleEntries() {
-        workingEntries.entrySet().removeIf(mapEntry -> mapEntry.getValue().getQuantity() < 1);
+        workingEntries.values().removeIf(purchaseEntry -> purchaseEntry.getQuantity() < 1);
     }
 
     public Purchase toPurchase() {
         clearStaleEntries();
-        workingPurchase.setPurchaseEntries(new HashSet<>(workingEntries.values()));
-        return workingPurchase;
+        Purchase purchase = new Purchase(customer);
+        Set<PurchaseEntry> mappedEntries = workingEntries.entrySet().stream()
+                .map(productAndEntry -> new PurchaseEntry(
+                        purchase,
+                        productAndEntry.getKey(),
+                        productAndEntry.getValue().getQuantity(),
+                        productAndEntry.getValue().getCurrentPrice()
+                ))
+                .collect(Collectors.toSet());
+        purchase.setPurchaseEntries(mappedEntries);
+        return purchase;
     }
 }
