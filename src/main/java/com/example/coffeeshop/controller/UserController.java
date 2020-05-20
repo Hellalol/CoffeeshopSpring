a2a562d23b4407b3dac20bb287e5672a23782beb
Fredrik Billing <frhabi@gmail.com>
2020-05-20 13:09:08 +0200

    Login and redirect works fine, logged in user's id gets stored in localstorage

---
+++ src/main/java/com/example/coffeeshop/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.example.coffeeshop.controller;
+
+import com.example.coffeeshop.domain.User;
+import com.example.coffeeshop.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/login")
+public class UserController {
+
+    public UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/{username}/{password}")
+    public Optional<User> getCurrentUser(@PathVariable String username, @PathVariable String password){
+        return userService.getCurrentUser(username,password);
+    }
+}
