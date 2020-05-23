5301ab5d719ecb4ff5f1673637efd97673991a75
Victor Olofsson <victor.olofsson@yh.nackademin.se>
2020-05-23 10:30:33 +0200

    PurchaseController is a mess, but we're stuck with it.

--- src/main/java/com/example/coffeeshop/controller/PurchaseController.java
+++ src/main/java/com/example/coffeeshop/controller/PurchaseController.java
@@ -13,17 +13,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import java.util.NoSuchElementException;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/purchase")
 // TODO Throw correct exceptions
 public class PurchaseController {
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(PurchaseController.class);
     private final PurchaseService purchaseService;
     private final ProductService productService;
     private final CustomerService customerService;
-    private static final org.slf4j.Logger log = LoggerFactory.getLogger(PurchaseController.class);
 
     @Autowired
     public PurchaseController(PurchaseService purchaseService, ProductService productService, CustomerService customerService) {
@@ -41,22 +41,21 @@ public class PurchaseController {
 
     @PostMapping("/new2/{id}")
     @ResponseStatus(HttpStatus.CREATED)
-    // TODO Replace input parameter with wherever we're actually getting the customer from
     public PurchaseDto create2(@PathVariable Long id) {
-        Customer customer = customerService.getCustomerById(id).get();
+        Customer customer = customerService.getCustomerById(id).orElseThrow(NoSuchElementException::new);
         return new PurchaseDto(purchaseService.getNewPurchase(customer));
     }
 
     @PostMapping("/{id}/checkout")
     public PurchaseDto checkout(@PathVariable long id) {
-        Purchase p = purchaseService.getById(id).orElseThrow();
+        Purchase p = purchaseService.getById(id).orElseThrow(NoSuchElementException::new);
         return new PurchaseDto(purchaseService.checkout(p));
     }
 
     @CrossOrigin()
     @GetMapping("/{id}")
     public PurchaseDto getPurchase(@PathVariable Long id) {
-        Purchase p = purchaseService.getById(id).orElseThrow();
+        Purchase p = purchaseService.getById(id).orElseThrow(NoSuchElementException::new);
         return new PurchaseDto(p);
     }
 
@@ -65,15 +64,16 @@ public class PurchaseController {
     // TODO Hash out how this and putAll are supposed to work, also figure out request mapping
     public PurchaseDto put(@PathVariable long id, @RequestBody PurchaseEntryDto entry) {
         // TODO Check id and purchase entry
-        Purchase purchase = purchaseService.getById(id).orElseThrow();
-        Product product = productService.getById(entry.getProductId()).orElseThrow();
-        return new PurchaseDto(purchaseService.setProductQuantity(purchase, product, entry.getQuantity()));
+        Purchase purchase = purchaseService.getById(id).orElseThrow(NoSuchElementException::new);
+        Product product = productService.getById(entry.getProductId()).orElseThrow(NoSuchElementException::new);
+        purchaseService.setProductQuantity(purchase, product, entry.getQuantity());
+        return new PurchaseDto(purchase);
     }
 
     @PostMapping("/{id}/addAll")
     // TODO Hash out how this and put are supposed to work, also figure out request mapping
     public PurchaseDto putAll(@PathVariable long id, @RequestBody Iterable<PurchaseEntryDto> entries) {
-        Purchase purchase = purchaseService.getById(id).orElseThrow();
+        Purchase purchase = purchaseService.getById(id).orElseThrow(NoSuchElementException::new);
         for (PurchaseEntryDto entry : entries) {
             put(id, entry);
         }
@@ -82,8 +82,8 @@ public class PurchaseController {
 
     @PostMapping("/{purchaseId}/remove/{productId}")
     public PurchaseDto remove(@PathVariable long purchaseId, @PathVariable long productId) {
-        Purchase purchase = purchaseService.getById(purchaseId).orElseThrow();
-        Product product = productService.getById(productId).orElseThrow();
+        Purchase purchase = purchaseService.getById(purchaseId).orElseThrow(NoSuchElementException::new);
+        Product product = productService.getById(productId).orElseThrow(NoSuchElementException::new);
         // TODO A remove() method might be more elegant
         return new PurchaseDto(purchaseService.setProductQuantity(purchase, product, 0));
     }
@@ -102,47 +102,36 @@ public class PurchaseController {
         return putAll(id, newPurchase.getPurchaseEntries());
     }
 
-
     @PostMapping("/{id}/addByOne/{productId}")
     public PurchaseDto addByOne(@PathVariable long id, @PathVariable Long productId) {
-        Optional<Product> product = productService.getById(productId);
-        Purchase purchase = purchaseService.getById(id).orElseThrow();
-        Product realProduct = purchase.getEntry(product.get()).getProduct();
-        int newQuantity = purchase.getEntry(product.get()).getQuantity() + 1;
-        purchaseService.setProductQuantity(purchase, realProduct, newQuantity);
+        Purchase purchase = purchaseService.getById(id).orElseThrow(NoSuchElementException::new);
+        Product product = productService.getById(productId).orElseThrow(NoSuchElementException::new);
+        purchaseService.setProductQuantity(purchase, product, purchase.getEntry(product).getQuantity() + 1);
         return new PurchaseDto(purchase);
     }
 
     @PostMapping("/{id}/subtractByOne/{productId}")
     public PurchaseDto subtractByOne(@PathVariable long id, @PathVariable Long productId) {
-        Optional<Product> product = productService.getById(productId);
-        Purchase purchase = purchaseService.getById(id).orElseThrow();
-        Product realProduct = purchase.getEntry(product.get()).getProduct();
-        int newQuantity = purchase.getEntry(product.get()).getQuantity() - 1;
-        purchaseService.setProductQuantity(purchase, realProduct, newQuantity);
+        Purchase purchase = purchaseService.getById(id).orElseThrow(NoSuchElementException::new);
+        Product product = productService.getById(productId).orElseThrow(NoSuchElementException::new);
+        purchaseService.setProductQuantity(purchase, product, purchase.getEntry(product).getQuantity() - 1);
         return new PurchaseDto(purchase);
     }
 
     @PostMapping("/{id}/removeProduct/{productId}")
     public PurchaseDto removeProduct(@PathVariable long id, @PathVariable Long productId) {
-        Optional<Product> product = productService.getById(productId);
         Purchase purchase = purchaseService.getById(id).orElseThrow();
-        Product realProduct = purchase.getEntry(product.get()).getProduct();
-        purchaseService.setProductQuantity(purchase, realProduct, 0);
+        Product product = productService.getById(productId).orElseThrow(NoSuchElementException::new);
+        purchaseService.setProductQuantity(purchase, product, 0);
         return new PurchaseDto(purchase);
     }
 
     @PostMapping("/{purchaseId}/addProductToPurches/{productId}")
-    public void addProductToPurches(@PathVariable Long purchaseId, @PathVariable Long productId) {
+    public PurchaseDto addProductToPurchase(@PathVariable Long purchaseId, @PathVariable Long productId) {
         //Metoden kallas efter nytt purchase ID har skapats
-        Optional<Purchase> purchase = purchaseService.getById(purchaseId);
-        Optional<Product> product = productService.getById(productId);
-        int newQuantity = purchase.get().getEntry(product.get()).getQuantity();
-
-        if (newQuantity > 0) {
-            purchaseService.setProductQuantity(purchase.get(), product.get(), newQuantity + 1);
-        } else {
-            purchaseService.addNewProduct(purchase.get(), product.get());
-        }
+        Purchase purchase = purchaseService.getById(purchaseId).orElseThrow(NoSuchElementException::new);
+        Product product = productService.getById(productId).orElseThrow(NoSuchElementException::new);
+        purchaseService.setProductQuantity(purchase, product, purchase.getEntry(product).getQuantity() + 1);
+        return new PurchaseDto(purchase);
     }
 }
